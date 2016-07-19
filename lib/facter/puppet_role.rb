eca7a8fadf28665480c3d9c2b1ad100283d14275
Josh Parrish <jparrish@astate.edu>
2016-07-19 13:45:56 -0500

    fix puppet_role

--- lib/facter/puppet_role.rb
+++ lib/facter/puppet_role.rb
@@ -1,8 +1,8 @@
 # ([a-z]+)[0-9]+, i.e. www01 or logger22 have a puppet_role of www or logger
-if Facter.value(:hostname) =~ /(classr)/
+if Facter.value(:hostname) =~ /(class)/
   Facter.add('puppet_role') do
     setcode do
-      $1
+      'classr'
     end
   end
 
