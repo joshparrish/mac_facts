952b5664d836489116b39ab6d5b441f3ad06213c
Josh Parrish <jparrish@astate.edu>
2016-07-20 10:21:49 -0500

    add facts for munki

---
+++ lib/facter/mac_munki_version.rb
@@ -0,0 +1,11 @@
+#mac_munki_version.rb
+Facter.add(:mac_munki_version) do
+  confine :kernel => "Darwin"
+  setcode do
+    if File.exist?('/usr/local/munki/managedsoftwareupdate')
+        Facter::Util::Resolution.exec("/usr/local/munki/managedsoftwareupdate --version")
+    else
+        "Munki not installed"
+    end
+  end
+end
